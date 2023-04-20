f686b0b1cb92dd8dc3e0f9cc186ff08e26d0a654
ajd428 <102680878+ajd428@users.noreply.github.com>
2023-04-20 00:05:44 -0400

    23

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,3 +1,9 @@
-import { gsap } from "gsap";
+import {gsap} from "gsap"
 
-gsap.from("#logo", {duration:4, aplha:0, delay:2});
+gsap.from("#logo", {duration:2.25, y:"-=80", delay:0.25});
+
+gsap.from("nav li", {duration:1.75, x:"+=450", stagger: 0.25, delay:0.25});
+
+gsap.from("#content", {duration:2.25, scale:1.5, rotation:180});
+
+gsap.from("#hero-content", {duration:0.75, yoyo:true, repeat:2, repeatDelay:0.05, scale:1.5});
