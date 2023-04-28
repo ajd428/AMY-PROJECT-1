c50cdf2365f397254e22217dd4ee3a662755a07b
ajd428 <102680878+ajd428@users.noreply.github.com>
2023-04-27 20:27:09 -0400

    3

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,4 +2,10 @@ import {gsap} from "gsap";
 
 //QUIZ
 
+let mainTl = gsap.timeline();
 
+nTl.from(".red", {duration:1, x:0})
+.from(".blue", {duration:1, x:350})
+.to(".red", {delay:2, duration:2, y:0})
+.to(".blue", {delay:2, duration2, y:300})
+;
