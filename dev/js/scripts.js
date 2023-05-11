dbca0cad6cbb9b3d848995b4953d01721cb2c56d
ajd428 <102680878+ajd428@users.noreply.github.com>
2023-05-11 12:26:56 -0400

    6

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,2 +1,18 @@
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
+gsap.registerPlugin(ScrollTrigger);
+
+function oneAni(){
+    let tl = gsap.timeline();
+    tl.from("#", {duration:0.25, alpha:0, y:-100})
+    .from("#", {duation:1, alpha: 0, y:-100})
+    return tl;
+
+}
+
+
+let mainTl = gsap.timeline();
+mainTl.add(oneAni())
+
+;
