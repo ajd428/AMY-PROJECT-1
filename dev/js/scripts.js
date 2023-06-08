566021b16a632d7d433ceea3c4716a918999f99d
ajd428 <102680878+ajd428@users.noreply.github.com>
2023-06-08 01:40:49 -0400

    4

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,18 @@
-//import { gsap } from "gsap";
+import { gsap } from "gsap";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import { TextPlugin } from "gsap/TextPlugin";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { GSDevTools } from "gsap/GSDevTools";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+import { MotionPathHelper } from "gsap/MotionPathHelper";
+
+gsap.registerPlugin(MotionPathPlugin, TextPlugin, DrawSVGPlugin, GSDevTools, MorphSVGPlugin, MotionPathHelper);
+
+const mainTL = gsap.timeline();
+
+mainTL.fromTo("#white-m", {drawSVG:"0%"}, {duration:2, drawSVG:"100%"})
+
+
+;
+
+GSDevTools.create();
