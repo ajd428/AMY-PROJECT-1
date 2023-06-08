01b75adf0ea6b9ccd1efbcb4ae64050d2b74f68c
ajd428 <102680878+ajd428@users.noreply.github.com>
2023-06-08 13:00:56 -0400

    5

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,20 @@
 //import { gsap } from "gsap";
+import { gsap } from "gsap";
+import { TextPlugin } from "gsap/TextPlugin";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { GSDevTools } from "gsap/GSDevTools";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+
+gsap.registerPlugin(TextPlugin, DrawSVGPlugin, GSDevTools, MorphSVGPlugin);
+
+const mainTL = gsap.timeline();
+mainTL.fromTo("single-sprout", {drawSVG:"0%"}, {duration:1, drawSVG: "100%"})
+.to("#single-sprout", {duration:1, morphSVG:"#leaf-sprout", fill: "#3D5A34"})
+.to("#single-sprout", {duration:1, morphSVG:"#vector-1, #vector-2, #vector-3, #vector-4"})
+.to("#single-sprout", {duration:1, morphSVG:"#vase"})
+.to("#single-sprout", {duration:1, morphSVG:"#plant"})
+//.to("#title", {duration:3, opacity:"100%"})
+;
+
+
+GSDevTools.create();
