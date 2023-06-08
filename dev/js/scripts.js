778b831d3cf3f433c3b85963794b294b3a89acd1
ajd428 <102680878+ajd428@users.noreply.github.com>
2023-06-08 16:15:17 -0400

    6

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -13,8 +13,53 @@ mainTL.fromTo("single-sprout", {drawSVG:"0%"}, {duration:1, drawSVG: "100%"})
 .to("#single-sprout", {duration:1, morphSVG:"#vector-1, #vector-2, #vector-3, #vector-4"})
 .to("#single-sprout", {duration:1, morphSVG:"#vase"})
 .to("#single-sprout", {duration:1, morphSVG:"#plant"})
-//.to("#title", {duration:3, opacity:"100%"})
+
+.to("#single-sprout", {fill:"#FF93B3", stroke:"#FF93B3"})
+.to("#the-t", {duration:.5, autoAlpha:1, fill:"#FF93B3", stroke:"#FF93B3"}, "<")
+.to("#single-sprout, #the-t", {fill:"#FCB140", stroke:"#FCB140"})
+.to("#the-h", {duration:.5, autoAlpha:1, fill:"#FCB140", stroke:"#FCB140"},"<")
+.to("#single-sprout, #the-t, #the-h", {fill:"#98B66B", stroke:"#98B66B"})
+.to("#the-e", {duration:.5, autoAlpha:1, fill:"#98B66B", stroke:"#98B66B"}, "<")
+
+.to("#b", {duration:.5, autoAlpha:1})
+.to("#single-sprout",{fill:"#FCB140", stroke:"#FCB140"},"<" )
+.to("#the-t, #the-h, #the-e",{fill:"#FCB140", stroke:"#FCB140"},"<" )
+
+.to("#o", {duration:.5, autoAlpha:1})
+.to("#single-sprout", {fill:"#98B66B", stroke:"#98B66B"}, "<")
+.to("#the-t, #the-h, #the-e",{fill:"#98B66B", stroke:"#98B66B"},"<" )
+
+.to("#t", {duration:.5, autoAlpha:1})
+.to("#single-sprout",{fill:"#FF93B3", stroke:"#FF93B3"},"<" )
+.to("#the-t, #the-h, #the-e",{fill:"#FF93B3", stroke:"#FF93B3"},"<" )
+
+.to("#a1", {duration:.5, autoAlpha:1})
+.to("#single-sprout",{fill:"#FCB140", stroke:"#FCB140"},"<" )
+.to("#the-t, #the-h, #the-e",{fill:"#FCB140", stroke:"#FCB140"},"<" )
+
+.to("#n", {duration:.5, autoAlpha:1})
+.to("#single-sprout", {fill:"#98B66B", stroke:"#98B66B"}, "<")
+.to("#the-t, #the-h, #the-e",{fill:"#98B66B", stroke:"#98B66B"},"<" )
+
+.to("#c", {duration:.5, autoAlpha:1})
+.to("#single-sprout",{fill:"#FF93B3", stroke:"#FF93B3"},"<" )
+.to("#the-t, #the-h, #the-e",{fill:"#FF93B3", stroke:"#FF93B3"},"<" )
+
+.to("#a2", {duration:.5, autoAlpha:1})
+.to("#single-sprout",{fill:"#FCB140", stroke:"#FCB140"},"<" )
+.to("#the-t, #the-h, #the-e",{fill:"#FCB140", stroke:"#FCB140"},"<" )
+
+.to("#l", {duration:.5, autoAlpha:1})
+.to("#single-sprout", {fill:"#98B66B", stroke:"#98B66B"}, "<")
+.to("#the-t, #the-h, #the-e",{fill:"#98B66B", stroke:"#98B66B"},"<" )
+
+
+//.to("#the-t", {duration:1, autoAlpha:0, fill: "#98B66B"})
 ;
 
 
 GSDevTools.create();
+
+
+//splitColor()
+//ease: Power2.easeInOut
